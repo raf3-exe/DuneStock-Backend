903dfd32eb388906cf3de33e014dc0fe5367ae0b
raf3_ <daas2277@gmail.com>
2026-02-08 22:28:25 +0700

    Create

---
+++ src/main/java/com/dunestock/api/DunestockApiApplication.java
@@ -0,0 +1,13 @@
+package com.dunestock.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DunestockApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DunestockApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dunestock/api/DunestockApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dunestock.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DunestockApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
